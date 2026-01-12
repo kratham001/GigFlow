157cefd7e0f3777f35ce21d85bbe2552857ed0a7
Kartikay dev <120315739+kratham001@users.noreply.github.com>
2026-01-12 17:05:29 +0000

    errors fixed

--- backend/models/Bid.js
+++ backend/models/Bid.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bidSchema = new mongoose.Schema({
   gigId: { type: mongoose.Schema.Types.ObjectId, ref: 'Gig', required: true },
   freelancerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  price: { type: Number, required: true }, // <--- NEW FIELD
+  amount: { type: Number, required: true }, 
   message: { type: String, required: true },
   status: { 
     type: String, 
