6369371edfde40bd577245c20d739b228b67b002
Kartikay dev <120315739+kratham001@users.noreply.github.com>
2026-01-12 20:00:43 +0000

    errors fixed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,7 +9,7 @@ const generateToken = (res, userId) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
-    sameSite: 'none',
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
